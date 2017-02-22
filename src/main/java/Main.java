3f15d7faba20cead6e2bafc3d31e764cdf5f7083
Andrew Schwartz <andrew@aschwartz.me>
2017-02-22 14:01:35 -0700

    add TODO, others

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,16 @@ import java.io.IOException;
 
 /**
  * Created by andrew on 2/21/17.
+ * TODO remove images from articles
+ * TODO edit spacing on articles for better reading
+ * TODO make a directory file for downloaded articles
+ * TODO fix titles--remove all chars but [a-zA-Z] and '-'
  */
 public class Main {
     public static void main(String[] args) {
         for (String s : GetLinks.googleLinkGetter("China Trump")) {
             Parser.Parse(s);
-            System.out.println(s);
+            // System.out.println(s);
         }
     }
 }
