ad4102e3cdf7d780c48dcc6bbc8705038e1ae2f9
Andrew Schwartz <andrew@aschwartz.me>
2017-02-22 22:27:07 -0700

    comment code

--- src/main/java/Vars.java
+++ src/main/java/Vars.java
@@ -1,7 +1,11 @@
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by andrew on 2/22/17.
  */
 public class Vars {
-    public static int linksCount;
-    public static int linksTotal;
+    public static int linksCount; // for pretty output
+    public static int linksTotal; // for pretty output
+    public static Set<String> articleNames = new HashSet<>(); // for holding original article's titles for directory
 }
